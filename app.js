d1fc577e506dfaeab400cae5d5ba5180f55d9b5e
Ragnar <ragnar.naruski@voco.ee>
2022-11-30 21:53:06 +0200

    change weather data from API

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const weather=new Weather('Tallinn')
 
 const ui=new UI()
 
+const form=document.querySelector('#change-city')
+form.addEventListener('submit',changeWeather)
+
+function changeWeather(event){
+    const city=document.querySelector('#city-name').value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector('#city-name').value=''
+    event.preventDefault()
+}
+
 function getWeather(){
     weather.getWeather()
         .then(data=>{
