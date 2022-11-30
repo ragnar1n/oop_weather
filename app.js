e8747c5153ba4c28acb0ad45f8d7a2eb783566f8
Ragnar <ragnar.naruski@voco.ee>
2022-11-30 21:39:00 +0200

    display weather data from API

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const weather=new Weather('Tallinn')
 
+const ui=new UI()
+
 function getWeather(){
     weather.getWeather()
         .then(data=>{
-
+            ui.drawWeather(data)
         })
         .catch(error=> console.log(error))
 }
